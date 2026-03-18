1f1c62b482ea16d2c71962957017ee339832e1d0
RANDRIAMAHEFA Liantsoa Alicia <167765196+liantsoaalicia@users.noreply.github.com>
2026-03-18 23:11:27 +0300

    Update insert_hotels.sql

--- script/insert_hotels.sql
+++ script/insert_hotels.sql
@@ -1,38 +1,38 @@
-INSERT INTO dev.lieux (code, libelle) VALUES
+INSERT INTO staging.lieux (code, libelle) VALUES
 ('AIR', 'Aéroport'),
 ('COL', 'Colbert'),
 ('NOV', 'Novotel'),
 ('IBI', 'Ibis'),
 ('LOK', 'Lokanga');
 
-INSERT INTO dev.hotel (nom, id_lieu) VALUES
-('Colbert', (SELECT id FROM dev.lieux WHERE code = 'COL')),
-('Novotel', (SELECT id FROM dev.lieux WHERE code = 'NOV')),
-('Ibis', (SELECT id FROM dev.lieux WHERE code = 'IBI')),
-('Lokanga', (SELECT id FROM dev.lieux WHERE code = 'LOK'));
+INSERT INTO staging.hotel (nom, id_lieu) VALUES
+('Colbert', (SELECT id FROM staging.lieux WHERE code = 'COL')),
+('Novotel', (SELECT id FROM staging.lieux WHERE code = 'NOV')),
+('Ibis', (SELECT id FROM staging.lieux WHERE code = 'IBI')),
+('Lokanga', (SELECT id FROM staging.lieux WHERE code = 'LOK'));
 
-INSERT INTO dev.distance ("from", "to", distance, unite) VALUES
+INSERT INTO staging.distance ("from", "to", distance, unite) VALUES
 -- AIR connections
-((SELECT id FROM dev.lieux WHERE code = 'AIR'), (SELECT id FROM dev.lieux WHERE code = 'COL'), 15.5, 'km'),
-((SELECT id FROM dev.lieux WHERE code = 'AIR'), (SELECT id FROM dev.lieux WHERE code = 'NOV'), 12.3, 'km'),
-((SELECT id FROM dev.lieux WHERE code = 'AIR'), (SELECT id FROM dev.lieux WHERE code = 'IBI'), 18.0, 'km'),
-((SELECT id FROM dev.lieux WHERE code = 'AIR'), (SELECT id FROM dev.lieux WHERE code = 'LOK'), 21.0, 'km'),
+((SELECT id FROM staging.lieux WHERE code = 'AIR'), (SELECT id FROM staging.lieux WHERE code = 'COL'), 15.5, 'km'),
+((SELECT id FROM staging.lieux WHERE code = 'AIR'), (SELECT id FROM staging.lieux WHERE code = 'NOV'), 12.3, 'km'),
+((SELECT id FROM staging.lieux WHERE code = 'AIR'), (SELECT id FROM staging.lieux WHERE code = 'IBI'), 18.0, 'km'),
+((SELECT id FROM staging.lieux WHERE code = 'AIR'), (SELECT id FROM staging.lieux WHERE code = 'LOK'), 21.0, 'km'),
 -- COL connections
-((SELECT id FROM dev.lieux WHERE code = 'COL'), (SELECT id FROM dev.lieux WHERE code = 'NOV'), 13.5, 'km'),
-((SELECT id FROM dev.lieux WHERE code = 'COL'), (SELECT id FROM dev.lieux WHERE code = 'IBI'), 10.0, 'km'),
-((SELECT id FROM dev.lieux WHERE code = 'COL'), (SELECT id FROM dev.lieux WHERE code = 'LOK'), 12.0, 'km'),
+((SELECT id FROM staging.lieux WHERE code = 'COL'), (SELECT id FROM staging.lieux WHERE code = 'NOV'), 13.5, 'km'),
+((SELECT id FROM staging.lieux WHERE code = 'COL'), (SELECT id FROM staging.lieux WHERE code = 'IBI'), 10.0, 'km'),
+((SELECT id FROM staging.lieux WHERE code = 'COL'), (SELECT id FROM staging.lieux WHERE code = 'LOK'), 12.0, 'km'),
 -- NOV connections
-((SELECT id FROM dev.lieux WHERE code = 'NOV'), (SELECT id FROM dev.lieux WHERE code = 'IBI'), 8.2, 'km'),
-((SELECT id FROM dev.lieux WHERE code = 'NOV'), (SELECT id FROM dev.lieux WHERE code = 'LOK'), 14.5, 'km'),
+((SELECT id FROM staging.lieux WHERE code = 'NOV'), (SELECT id FROM staging.lieux WHERE code = 'IBI'), 8.2, 'km'),
+((SELECT id FROM staging.lieux WHERE code = 'NOV'), (SELECT id FROM staging.lieux WHERE code = 'LOK'), 14.5, 'km'),
 -- IBI-LOK connection
-((SELECT id FROM dev.lieux WHERE code = 'IBI'), (SELECT id FROM dev.lieux WHERE code = 'LOK'), 9.5, 'km');
+((SELECT id FROM staging.lieux WHERE code = 'IBI'), (SELECT id FROM staging.lieux WHERE code = 'LOK'), 9.5, 'km');
 
 
-INSERT INTO dev.param (cle, valeur) VALUES
-('vm', '60'),
+INSERT INTO staging.param (cle, valeur) VALUES
+('vm', '50'),
 ('ta', '30');
 
--- INSERT INTO dev.vehicule (reference, place, type_carburant) VALUES
+-- INSERT INTO staging.vehicule (reference, place, type_carburant) VALUES
 -- ('CAR-5D',    5, 'D'),
 -- ('CAR-5E',    5, 'E'),
 -- ('VAN-8D',    8, 'D'),
@@ -41,7 +41,7 @@ INSERT INTO dev.param (cle, valeur) VALUES
 -- ('BUS-15E',  15, 'E'),
 -- ('BUS-20E',  20, 'E');
 
--- INSERT INTO dev.reservation(id_client, nb_passager, date_heure_arrivee, id_hotel) VALUES
+-- INSERT INTO staging.reservation(id_client, nb_passager, date_heure_arrivee, id_hotel) VALUES
 -- ('A02', 3, '2026-03-10 08:00', 1),
 -- ('A01', 2, '2026-03-10 08:00', 1),
 -- ('B01', 4, '2026-03-10 08:00', 2),
@@ -49,7 +49,7 @@ INSERT INTO dev.param (cle, valeur) VALUES
 -- ('C01', 6, '2026-03-10 08:00', 3),
 -- ('C02', 4, '2026-03-10 08:00', 3);
 
--- INSERT INTO dev.reservation(id_client, nb_passager, date_heure_arrivee, id_hotel) VALUES
+-- INSERT INTO staging.reservation(id_client, nb_passager, date_heure_arrivee, id_hotel) VALUES
 -- ('D01', 11, '2026-03-10 11:00', 4),
 -- ('D02',  4, '2026-03-10 11:00', 4),
 -- ('D03',  8, '2026-03-10 11:00', 4),
@@ -61,7 +61,7 @@ INSERT INTO dev.param (cle, valeur) VALUES
 -- ('H01', 10, '2026-03-12 08:30', 3),
 -- ('H02', 6,  '2026-03-12 08:30', 3);
 
--- INSERT INTO dev.reservation(id_client, nb_passager, date_heure_arrivee, id_hotel) VALUES
+-- INSERT INTO staging.reservation(id_client, nb_passager, date_heure_arrivee, id_hotel) VALUES
 -- ('A02', 3, '2026-03-10 08:00', 1),
 -- ('A01', 2, '2026-03-10 08:00', 1),
 -- ('B01', 4, '2026-03-10 08:00', 2),
@@ -74,12 +74,29 @@ INSERT INTO dev.param (cle, valeur) VALUES
 -- ('E01', 12, '2026-03-10 14:00', 1),
 -- ('E02',  2, '2026-03-10 14:00', 1);
 
-INSERT INTO dev.vehicule (reference, place, type_carburant) VALUES
-('CAR-5D', 10, 'D'),
-('CAR-5E', 9, 'E');
+-- INSERT INTO staging.vehicule (reference, place, type_carburant) VALUES
+-- ('CAR-5D', 10, 'D'),
+-- ('CAR-5E', 9, 'E');
 
-INSERT INTO dev.reservation(id_client, nb_passager, date_heure_arrivee, id_hotel) VALUES
-('A02', 5, '2026-03-10 08:00', 1),
-('A01', 3, '2026-03-10 08:15', 2),
-('B01', 3, '2026-03-10 08:45', 2),
-('A03', 8, '2026-03-10 09:00', 1);
+-- INSERT INTO staging.reservation(id_client, nb_passager, date_heure_arrivee, id_hotel) VALUES
+-- ('A02', 5, '2026-03-10 08:00', 1),
+-- ('A01', 3, '2026-03-10 08:15', 2),
+-- ('B01', 3, '2026-03-10 08:45', 2),
+-- ('A03', 8, '2026-03-10 09:00', 1);
+
+INSERT INTO staging.vehicule (reference, place, type_carburant) VALUES
+('VAN-8D-A', 8, 'D'),
+('VAN-8E-B', 8, 'E'),
+('VAN-10D', 10, 'D');
+
+INSERT INTO staging.reservation(id_client, nb_passager, date_heure_arrivee, id_hotel) VALUES
+-- 08:00 -> devrait prendre un 8 places (capacite la plus proche), pas le 10 places
+('T01', 7, '2026-03-10 08:00', (SELECT id FROM staging.hotel WHERE nom = 'Lokanga')),
+-- 08:05 -> 2eme reservation quasi simultanee, prend l'autre 8 places
+('T02', 9, '2026-03-10 08:05', (SELECT id FROM staging.hotel WHERE nom = 'Ibis')),
+-- 08:20 -> meme fenetre ta=30, les deux 8 places peuvent etre occupes selon retour -> test contrainte trajet
+-- ('T03', 7, '2026-03-10 08:20', (SELECT id FROM staging.hotel WHERE nom = 'Lokanga')),
+-- 11:00 -> les vehicules sont revenus, re-test du best-fit sur un nouveau slot
+('T04', 7, '2026-03-10 11:00', (SELECT id FROM staging.hotel WHERE nom = 'Novotel')),
+-- 11:00 -> petit groupe pour verifier que l'assignation remplit le meme vehicule si place restante
+('T05', 1, '2026-03-10 11:00', (SELECT id FROM staging.hotel WHERE nom = 'Colbert'));
